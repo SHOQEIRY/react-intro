d68e34c214afa6aac911de78f93c0b33448ee28d
SHOQEIRY <shoqeiry@gmail.com>
2023-01-09 20:07:41 +0200

    first code

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+        <img class="image" src="/assets/gmc.svg"/>
+
+    <div class="form-floating mb-3">
+  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"></input>
+  <label for="floatingInput">Email address</label>
+</div>
+<div class="form-floating">
+  <input type="password" class="form-control" id="floatingPassword" placeholder="Password"></input>
+  <label for="floatingPassword">Password</label>
+  <button type="button" class="btn btn-danger">Sign-in</button>
+    
+</div>
+
+
+    </>
   );
 }
 
